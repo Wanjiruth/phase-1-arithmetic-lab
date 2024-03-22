c4bb2fec799b3be25d6b0e3de4869f8c1704f841
wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-03-22 13:02:52 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; 
+console.log(multiply); 
+
+const random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(5, 10, 20, 15);
+console.log(max); 
+
